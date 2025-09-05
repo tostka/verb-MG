0b20163bb00de32b99d4840dece830a05101f005
Tostka <tostka@users.noreply.github.com>
2025-09-05 15:57:13 -0500

    Update get-MGCodeCmdletPermissionsTDO.ps1
    * 3:49 PM 9/5/2025 FUNDEMENTAL FLAW: find-mgGraphCommand DOES NOT HAVE ALL MGGRAPH CMDLET PERMS! And it doesn't throw errors on empty perms, it just returns NOTHING
                So I coded in some low-brow post empty-perm sanity checks: If the cmd has -confim/-force/-whatif, it's likely potentially destructive.
                If it has parameters on psScriptAnalyzer's change trigger verbv list: New, Set, Remove, Start, Stop, Restart, Reset, and Update - likewise
                Outputs a warning to manually lookup the M$ Dox Permissions table for each cmdlet.

--- Public/get-MGCodeCmdletPermissionsTDO.ps1
+++ Public/get-MGCodeCmdletPermissionsTDO.ps1
@@ -21,6 +21,10 @@
         AddedWebsite: 
         AddedTwitter: 
         REVISIONS
+        * 3:49 PM 9/5/2025 FUNDEMENTAL FLAW: find-mgGraphCommand DOES NOT HAVE ALL MGGRAPH CMDLET PERMS! And it doesn't throw errors on empty perms, it just returns NOTHING
+            So I coded in some low-brow post empty-perm sanity checks: If the cmd has -confim/-force/-whatif, it's likely potentially destructive. 
+            If it has parameters on psScriptAnalyzer's change trigger verbv list: New, Set, Remove, Start, Stop, Restart, Reset, and Update - likewise
+            Outputs a warning to manually lookup the M$ Dox Permissions table for each cmdlet.
         * 1:49 PM 5/14/2025 add: -cmdlets, bypasses AST parsing cuts right to find-mgGraphCommand expansion; additional verbose status echos (as it's returning very limited set of perms)
         * 4:37 PM 5/12/2025 retweaked expansion; found that the cmdlet name filtering wasn't working as a raw [regex], had to .tostring() the regex to get it to return more than a single item
         * 12:23 PM 5/6/2025 wrapper for verb-dev\get-codeprofileAST() that parses [verb]-MG[noun] cmdlets from a specified -file or -scriptblock, and reseolves the necessary connect-mgGraph delegated access -scope permissions, using the Find-MgGraphCommand command.
@@ -171,6 +175,7 @@
                 if($verbose){if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level VERBOSE } 
                 else{ write-verbose "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; } ; 
                 write-host -NoNewline '.' ; 
+                $thisCommand = get-command -Name $thisCmdlet -EA STOP
                 #$PermsRqd += Find-MgGraphCommand -command $thisCmdlet -ea 0| Select -First 1 -ExpandProperty Permissions | Select -Unique name ; 
                 $thisPerm = $null ; 
                 $thisPerm = Find-MgGraphCommand -command $thisCmdlet -ea 0| Select -First 1 -ExpandProperty Permissions | Select -Unique name ; 
@@ -178,11 +183,50 @@
                     $PermsRqd += $thisPerm ; 
                     $smsg = "(Find-MgGraphCommand -command $($thisCmdlet) returned Permissions:`n$(($thisPerm -join ','|out-string).trim()))" ; 
                 }else {
-                    $smsg = "($($Cmdlet):no Permissions returned" ; 
-                } ; 
-                if($verbose){if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level VERBOSE } 
-                else{ write-verbose "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; } ; 
+                    $smsg = "($($Cmdlet):no Permissions returned (by Microsoft.Graph.Authentication\Find-MgGraphCommand())" ; 
+                    <# 11:27 AM 9/5/2025 issues: Find-MgGraphCommand IS MISSING A TON OF CMDLET PERMS!
+                    remove-mgdomain is clearly destructive, but returns no permissions in the cmd:
 
+                    [How to get all Graph API permissions required to run selected code using PowerShell](https://doitpshway.com/how-to-get-all-graph-api-permissions-required-to-run-selected-code-using-powershell)
+                    Drawbacks ( to relying on Find-MgGraphCommand):
+                    * Official command Find-MgGraphCommand is used to translate command/URI calls to required permissions, 
+                            doesn't contain permissions for all commands/URIs (but this will be hopefully solved in the future)
+                    * Returns all permissions that can be used, not just the least one (but I am trying to solve this on my own)
+                    * URIs passed via parameter splatting or through variables aren't detected right now
+                    * but you will be notified about such issue, so you can solve this manually       
+                    
+                    built deps etc for: Get-CodeGraphPermissionRequirement
+                    and ran it: much more code, same garbage output:
+                    #-=-=-=-=-=-=-=-=
+                    VERBOSE: 'remove-mgdomain' doesn't need any permissions?!                        
+
+                    simple test on noperms:
+                    - does it have -confirm|-force|-whatif support? => implies it's potentially destructive, and shouuld require *some* kind of perms, throw an error back
+                    #>
+                    $riskParams = @() ; 
+                    $rgxShouldProcParams = 'confirm|force|whatif'
+                    if($hasShouldProcParams = $thisCommand.parameters.keys -match $rgxShouldProcParams){
+                        $riskParams = @($hasShouldProcParams)
+                        $smsg += "`nBUT!:$($thisCmdlet) has following risk-reflecting parameters:$($hasShouldProcParams -join ',') " ; ; 
+                    } ; 
+                    $verbsPSUseShouldProcessForStateChangingFunctions = @('New','Set','Remove','Start','Stop','Restart','Reset','and Update') ; 
+                    $rgxverbsPSUseShouldProcessForStateChangingFunctions = [regex]$rgx = ('(' + (($verbsPSUseShouldProcessForStateChangingFunctions |%{[regex]::escape($_)}) -join '|') + ')') ;
+                    if($StateChgParams = $thisCommand.parameters.keys -match $rgxverbsPSUseShouldProcessForStateChangingFunctions){
+                        $riskParams = $(@($riskParams);@($StateChgParams)) ; 
+                        $smsg += "`nBUT!:$($thisCmdlet) has following ShouldProcessForStateChanging parameters:$($riskparams -join ',') " ; ; 
+                    } ; 
+                    if($riskParams){
+                        $smsg += "`nFind-MgGraphCommand null Permissions return is likely *WRONG*: you should search for the doc page for:$($thisCmdlet), and manually assemble the connect-mgGraph -scope array from those specifications." ;
+                        $smsg += "`n(Find the Description: Permissions table, for the *accurate* perms for the cmdlet)" ;
+                    } ; 
+                } ; 
+                if($riskParams){
+                    if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level WARN -Indent} 
+                    else{ write-WARNING "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; 
+                }else{
+                    if($verbose){if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level VERBOSE } 
+                    else{ write-verbose "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; } ; 
+                } ; 
             } ; 
             write-host -foregroundcolor yellow "]" ; 
             $PermsRqd = $PermsRqd.name | select -unique ;
