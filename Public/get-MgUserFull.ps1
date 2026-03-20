3bcff140f9e7293b84dc79eedb94c07c508cb681
Tostka <tostka@users.noreply.github.com>
2026-03-20 14:45:17 -0500

    Update get-MgUserFull.ps1
    * 2:45 PM 3/20/2026 replaced connect-mg

--- Public/get-MgUserFull.ps1
+++ Public/get-MgUserFull.ps1
@@ -115,7 +115,7 @@
                 'mobilePhone','businessPhones',
                 'preferredLanguage'
             ) | select -unique ; 
-
+            #region MG_CONNECT ; #*------v MG_CONNECT v------
             #$isMgConn = [boolean]( (gcm get-mgcontext -ea 0) -AND (get-mgcontext -ea 0 )); if(-not $isMgConn ){connect-mg }else{write-verbose "MG connected"};
             if(-not (get-command  test-mgconnection)){
                 if(-not (get-module -list Microsoft.Graph -ea 0)){
@@ -129,118 +129,6 @@
             if($MGCntxt.isConnected){}else {
                 connect-mg ; 
             }
-            <#
-            #region cMG_SCAFFOLD ; #*------v cMG_SCAFFOLD v------
-            if(-not (get-command  test-mgconnection)){
-                if(-not (get-module -list Microsoft.Graph -ea 0)){
-                    $smsg = "MISSING Microsoft.Graph!" ; 
-                    $smsg += "`nUse: install-module Microsoft.Graph -scope CurrentUser" ;
-                    if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level WARN -Indent} 
-                    else{ write-WARNING "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; 
-                } ;             
-            } ;
-            $MGCntxt = test-mgconnection -Verbose:($VerbosePreference -eq 'Continue') ;
-            $o365Cred = $null ;
-            if($Credential -AND $MGCntxt.isConnected){
-                $smsg = "Explicit -Credential:$($Credential.username) -AND `$MGCntxt.isConnected: running pre:Disconnect-MgGraph" ; 
-                if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info } 
-                else{ write-host -foregroundcolor green "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ;
-                # Dmg returns a get-mgcontext into pipe, if you don't cap it corrupts the pipe on your current flow
-                $dOut = Disconnect-MgGraph -Verbose:($VerbosePreference -eq 'Continue')
-                $MGCntxt = test-mgconnection -Verbose:($VerbosePreference -eq 'Continue') ;
-            };
-            if($Credential){
-                $smsg = "`Credential:Explicit credentials specified, deferring to use..." ;
-                if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info }
-                else{ write-host -foregroundcolor green "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ;
-                write-verbose "get-TenantCredentials() return format: (emulating)" ; 
-                $o365Cred = [ordered]@{
-                    Cred=$Credential ;
-                    credType=$null ;
-                } ;
-                $uRoleReturn = resolve-UserNameToUserRole -UserName $Credential.username -verbose:$($VerbosePreference -eq "Continue") ; # Username
-                write-verbose "w full cred opt: $uRoleReturn = resolve-UserNameToUserRole -Credential $Credential -verbose = $($VerbosePreference -eq 'Continue')"  ; 
-                if($uRoleReturn.UserRole){
-                    $o365Cred.credType = $uRoleReturn.UserRole ;
-                } else {
-                    $smsg = "Unable to resolve `$credential.username ($($credential.username))"
-                    $smsg += "`nto a usable 'UserRole' spec!" ;
-                    if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level WARN } #Error|Warn|Debug
-                    else{ write-WARNING "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ;
-                    throw $smsg ;
-                    Break ;
-                } ;
-            } else {
-                if($MGCntxt.isConnected){
-                    if($MgCntxt.isUser){
-                        $TenantTag = $TenOrg = get-TenantTag -Credential $MgCntxt.Account ;
-                        $uRoleReturn = resolve-UserNameToUserRole -UserName $MgCntxt.CertificateThumbprint -verbose:$($VerbosePreference -eq "Continue") ;
-                        $credential = get-TenantCredentials -TenOrg $TenOrg -UserRole $uRoleReturn.UserRole -verbose:$($VerbosePreference -eq "Continue") ;
-                    } elseif($MgCntxt.isCBA -AND $MgCntxt.AppName -match 'CBACert-(\w{3})'){
-                            #$MgCntxt.AppName.split('-')[-1]
-                            $TenantTag = $TenOrg = $matches[1]
-                            # also need credential
-                            $uRoleReturn = resolve-UserNameToUserRole -UserName $MgCntxt.CertificateThumbprint -verbose:$($VerbosePreference -eq "Continue") ;
-                            write-verbose "ret'd obj:$uRoleReturn = [ordered]@{     UserRole = $null ;     Service = $null ;     TenOrg = $null ; } " ;  
-                            $credRet = get-TenantCredentials -TenOrg $TenOrg -UserRole $uRoleReturn.UserRole -verbose:$($VerbosePreference -eq "Continue")
-                            $credential = $credRet.Cred ;
-                    }else{
-                        $smsg = "UNABLE TO RESOLVE mgContext to a working TenOrg!" ;
-                        if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level WARN -Indent}
-                        else{ write-WARNING "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ;
-                    }
-                } ; 
-                $pltGTCred=@{TenOrg=$TenOrg ; UserRole=$null; verbose=$($verbose)} ;
-                if($UserRole){
-                    $smsg = "(`$UserRole specified:$($UserRole -join ','))" ;
-                    if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info }
-                    else{ write-host -foregroundcolor green "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ;
-                    $pltGTCred.UserRole = $UserRole;
-                } else {
-                    $smsg = "(No `$UserRole found, defaulting to:'CSVC','SID' " ;
-                    if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info }
-                    else{ write-host -foregroundcolor green "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ;
-                    $pltGTCred.UserRole = 'CSVC','SID' ;
-                } ;
-                $smsg = "get-TenantCredentials w`n$(($pltGTCred|out-string).trim())" ;
-                if($verbose){if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level verbose }
-                else{ write-verbose "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; } ;
-                $o365Cred = get-TenantCredentials @pltGTCred
-            } ;
-            if($o365Cred.credType -AND $o365Cred.Cred -AND $o365Cred.Cred.gettype().fullname -eq 'System.Management.Automation.PSCredential'){
-                $smsg = "(validated `$o365Cred contains .credType:$($o365Cred.credType) & `$o365Cred.Cred.username:$($o365Cred.Cred.username)" ;
-                if($verbose){if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level VERBOSE }
-                else{ write-verbose "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; } ;
-                write-verbose "populate $credential with return, if not populated (may be required for follow-on calls that pass common $Credentials through)" ; 
-                if((gv Credential) -AND $Credential -eq $null){
-                    $credential = $o365Cred.Cred ;
-                }elseif($credential.gettype().fullname -eq 'System.Management.Automation.PSCredential'){
-                    $smsg = "(`$Credential is properly populated; explicit -Credential was in initial call)" ; 
-                    if($verbose){if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level VERBOSE } 
-                    else{ write-verbose "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; } ; 
-                } else {
-                    $smsg = "`$Credential is `$NULL, AND $o365Cred.Cred is unusable to populate!" ;
-                    $smsg = "downstream commands will *not* properly pass through usable credentials!" ;
-                    if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level WARN -Indent}
-                    else{ write-WARNING "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ;
-                    throw $smsg ;
-                    break ;
-                } ;
-            } else {
-                $smsg = "UNABLE TO RESOLVE FUNCTIONAL CredType/UserRole from specified explicit -Credential:$($Credential.username)!" ;
-                if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level WARN -Indent}
-                else{ write-WARNING "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ;
-                break ;
-            } ;         
-            $pltCMG = [ordered]@{
-                Credential = $Credential ;
-                verbose = $($VerbosePreference -eq "Continue")  ;
-            } ;
-            if((get-command Connect-MG).Parameters.keys -contains 'silent'){
-                $pltCMG.add('Silent',$silent) ;
-            } ;
-            #endregion cMG_SCAFFOLD ; #*------^ END cMG_SCAFFOLD ^------
-            #>
             if(-not (get-command Get-MgUser)){
                 $smsg = "Missing Get-MgUser!" ; 
                 $smsg += "`nPre-connect to Microsoft.Graph via:" ;
@@ -249,6 +137,7 @@
                 else{ write-WARNING "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; 
                 BREAK ; 
             } ; 
+            #endregion MG_CONNECT ; #*------^ END MG_CONNECT ^------
             #region IS_PIPELINE ; #*------v IS_PIPELINE v------
             # check if using Pipeline input or explicit params:
             if ($PSCmdlet.MyInvocation.ExpectingInput) {
